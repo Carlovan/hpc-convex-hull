353685ef58500af8062d98d1b57c8fa3c87b2044
Carlovan <giulio.carlassare@gmail.com>
2020-03-31 13:40:30 +0200

    Time using different thread counts

--- scripts/time_all.sh
+++ scripts/time_all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 inputs=$PWD/inputs/*
+NPROC=`nproc`
 
 echo "executable,input,time,threads"
 
@@ -8,10 +9,16 @@ for e in $PWD/build/*; do
     baseEx=`basename $e`
     for i in $inputs; do
         baseIn=`basename $i`
-        echo -n "$baseEx,$baseIn,"
-        $e < $i 2>&1 > /dev/null | grep -E "Elapsed|Threads" |
-            sed -E 's/^.*time: (.*)$/\1/' |
-            sed -E 's/^.*Threads: (.*)$/\1/' |
-            paste -sd ','
+        maxThreads=1
+        if [[ $baseEx = omp-* ]]; then
+            maxThreads=$NPROC
+        fi
+        for threads in `seq 1 $maxThreads`; do
+            echo -n "$baseEx,$baseIn,"
+            OMP_NUM_THREADS=$threads $e < $i 2>&1 > /dev/null | grep -E "Elapsed|Threads" |
+                sed -E 's/^.*time: (.*)$/\1/' |
+                sed -E 's/^.*Threads: (.*)$/\1/' |
+                paste -sd ','
+        done
     done
 done
