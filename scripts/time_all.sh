a0848ee4bc67e7657be52cf9da83a5ea976138f0
Carlovan <giulio.carlassare@gmail.com>
2020-04-02 10:51:42 +0200

    Some fixes in timing script

--- scripts/time_all.sh
+++ scripts/time_all.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 inputs=$PWD/inputs/*
+executables=$PWD/build/*
 NPROC=`nproc`
 EVALS=5
 
 echo "executable,input,time,threads"
 
-for ex in $PWD/build/*; do
+for ex in $executables; do
     baseEx=`basename $ex`
     for inFile in $inputs; do
         baseIn=`basename $inFile`
@@ -18,13 +19,14 @@ for ex in $PWD/build/*; do
             if [[ $baseEx = omp-* ]]; then
                 export OMP_NUM_THREADS=$threads
             fi
+            cmd=$ex
             if [[ $baseEx = mpi-* ]]; then
-                ex="mpirun -n $threads $ex"
+                cmd="mpirun -n $threads $cmd"
             fi
 
             echo -n "$baseEx,$baseIn,"
-            for _ in `seq 1 $((EVALS + 2))`; do
-                $ex < $inFile 2>&1 > /dev/null | grep -E "Elapsed" | sed -E 's/^.*time: (.*)$/\1/'
+            for _ in `seq 1 $((EVALS))`; do
+                $ex < $inFile 2>&1 > /dev/null | grep -E "Elapsed" | sed -E 's/^.*time: +(.*)$/\1/'
             done | sort -n | tail -n +2 | head -n -1 | awk '{x+=$1; next} END{printf "%.7f", x/NR}' | tr -d '\n'
             echo ",$threads"
         done
