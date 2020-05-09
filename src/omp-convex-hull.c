b9bee8f2884a74a28808f936fd7eec3a34fb3772
Carlovan <giulio.carlassare@gmail.com>
2020-05-09 17:18:42 +0200

    Removed useless versions

--- src/omp-convex-hull.c
+++ src/omp-convex-hull.c
@@ -1,11 +1,11 @@
 /****************************************************************************
  *
- * omp-convex-hull.c
+ * convex-hull.c
  *
- * Compute the convex hull of a set of points in 2D, parallelized with OpenMP
+ * Compute the convex hull of a set of points in 2D
  *
  * Copyright (C) 2020 Giulio Carlassare <giulio.carlassare(at)studio.unibo.it>
- * Last updated on 2020-03-28
+ * Last updated on 2020-05-05
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,16 +23,17 @@
  ****************************************************************************
  *
  * Questo programma calcola l'inviluppo convesso (convex hull) di un
- * insieme di punti 2D letti da standard input usando l'algoritmo
- * "gift wrapping". Le coordinate dei vertici dell'inviluppo sono
- * stampate su standard output.  Per una descrizione completa del
- * problema si veda la specifica del progetto sul sito del corso:
+ * insieme di punti 2D letti da standard input con l'algoritmo "gift
+ * wrapping";  è stato usato OpenMP per parallelizzare la soluzione.
+ *   Le coordinate dei vertici dell'inviluppo con il minor numero di
+ * punti sono stampate su standard output.  Si veda la specifica del
+ * progetto sul sito del corso per una descrizione completa:
  *
  * http://moreno.marzolla.name/teaching/HPC/
  *
  * Per compilare:
  *
- * gcc -D_XOPEN_SOURCE=600 -std=c99 -Wall -Wpedantic -O2 convex-hull.c -o convex-hull -lm
+ * gcc -D_XOPEN_SOURCE=600 -std=c99 -Wall -Wpedantic -O2 omp-convex-hull.c -o omp-convex-hull -lm
  *
  * (il flag -D_XOPEN_SOURCE=600 e' superfluo perche' viene settato
  * nell'header "hpc.h", ma definirlo tramite la riga di comando fa si'
@@ -41,7 +42,7 @@
  *
  * Per eseguire il programma si puo' usare la riga di comando:
  *
- * ./convex-hull < ace.in > ace.hull
+ * ./omp-convex-hull < ace.in > ace.hull
  * 
  * Per visualizzare graficamente i punti e l'inviluppo calcolato è
  * possibile usare lo script di gnuplot (http://www.gnuplot.info/)
@@ -53,42 +54,31 @@
 #include "hpc.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
+
 #include "ioutils.h"
 #include "data_types.h"
 #include "mathutils.h"
-#include <omp.h>
 
-/**
- * Returns true if b is better than a
- */
-inline bool better_point(const point_t cur, const point_t a, const point_t b) {
+/* If every thread works on less than `copyThreshold` points,
+ * then they are not copied */
+const int copyThreshold = 10000;
+
+bool better_point(const point_t cur, const point_t a, const point_t b) {
     int t = turn(cur, a, b);
     return t == LEFT || (t == COLLINEAR && consecutive_dot_prod(cur, a, b) > 0);
 }
-
-typedef struct {
-    const point_t *point;
-    const point_t * const cur;
-} reduction_value_t;
-
-#pragma omp declare reduction ( best_point : reduction_value_t : \
-        omp_out.point = better_point(*omp_out.cur, *omp_out.point, *omp_in.point) \
-            ? omp_in.point : omp_out.point )\
-            initializer ( omp_priv = omp_orig )
-
-#pragma omp declare reduction ( leftmost_point : const point_t* : \
-        omp_out = omp_in->x < omp_out->x ? omp_in : omp_out ) initializer ( omp_priv = omp_orig )
 
 /**
  * Compute the convex hull of all points in pset using the "Gift
  * Wrapping" algorithm. The vertices are stored in the hull data
  * structure, that does not need to be initialized by the caller.
  */
-void convex_hull(const points_t *pset, points_t *hull)
-{
+void convex_hull(const points_t *pset, points_t *hull) {
     const int n = pset->n;
-    const point_t *p = pset->p;
+    int i;
+    int cur, leftmost;
 
     hull->n = 0;
     /* There can be at most n points in the convex hull. At the end of
@@ -96,46 +86,94 @@ void convex_hull(const points_t *pset, points_t *hull)
     hull->p = (point_t*)malloc(n * sizeof(*(hull->p))); assert(hull->p);
     
     /* Identify the leftmost point p[leftmost] */
-    const point_t *leftmostP = &p[0];
-    #pragma omp parallel for reduction(leftmost_point:leftmostP)
-    for (int i = 1; i<n; i++) {
-        if (p[i].x < leftmostP->x) {
-            leftmostP = &p[i];
+    leftmost = 0;
+    for (i = 1; i<n; i++) {
+        if (pset->p[i].x < pset->p[leftmost].x) {
+            leftmost = i;
         }
     }
-    int leftmost = leftmostP - p;
-    int cur = leftmost;
+    cur = leftmost;
+
+    /* Retrieve number of threads to allocate enough memory
+       for reduced values of individual threads */
+    int num_threads;
+    #pragma omp parallel
+    #pragma omp master
+    num_threads = omp_get_num_threads();
+
+    int* results = (int*)malloc(num_threads * sizeof(int));
+    assert(results);
     
-    /* Main loop of the Gift Wrapping algorithm. This is where most of
-       the time is spent; therefore, this is the block of code that
-       must be parallelized. */
-    do {
-        /* Add the current vertex to the hull */
-        assert(hull->n < n);
-        hull->p[hull->n] = p[cur];
-        hull->n++;
-        
-        /* Search for the next vertex */
-        reduction_value_t next = {&p[(cur + 1) % n], &p[cur]};
-        #pragma omp parallel for reduction(best_point:next)
-        for (int j=0; j<n; j++) {
-            if (better_point(p[cur], *next.point, p[j])) {
-                next.point = &p[j];
-            }
+    #pragma omp parallel default(none) firstprivate(n, num_threads) shared(pset, cur, hull, leftmost, results)
+    {
+        /* Points are equally subdivided between threads */
+        const int id = omp_get_thread_num();
+        const int part_size = (n + num_threads - 1) / num_threads;
+        const int start = part_size * id;
+        const int end = min(n, start + part_size);
+        const int memsize = (end-start)*sizeof(point_t);
+
+        /* If there are enough points, every thread copies the data it will work with
+           in its own local memory (assuming there are multiple NUMA nodes). */
+        point_t *p;
+        bool copied = false;
+        if (part_size > copyThreshold) {
+            p = (point_t*)malloc(memsize); assert(p);
+            memcpy(p, pset->p + start, memsize);
+            copied = true;
+        } else {
+            p = &pset->p[start];
         }
-        int nextI = next.point - p;
-        assert(cur != nextI);
-        cur = nextI;
-    } while (cur != leftmost);
+
+        /* Actual Gift Wrapping algorithm */
+        do {
+            /* Add the last found point to the convex hull */
+            #pragma omp single
+            {
+                assert(hull->n < n);
+                hull->p[hull->n] = pset->p[cur];
+                hull->n++;
+            }
+
+            /* Search for the next vertex; every thread works on its partition */
+            point_t curP = pset->p[cur];
+            int next = start;
+            for (int j=start+1; j<end; j++) {
+                if(next == cur || better_point(curP, p[next-start], p[j-start])) {
+                    next = j;
+                }
+            }
+            /* Save locally reduced value in common array */
+            results[id] = next;
+
+            /* Serially reduce the results of individual threads */
+            #pragma omp barrier
+            #pragma omp single
+            {
+                next = results[0];
+                for(int j = 1; j < num_threads; j++) {
+                    if (better_point(curP, pset->p[next], pset->p[results[j]])) {
+                        next = results[j];
+                    }
+                }
+                assert(cur != next);
+                cur = next;
+            }
+        } while (cur != leftmost);
+
+        /* If there were few points we didn't copy */
+        if (copied) {
+            free(p);
+        }
+    }
+    free(results);
     
     /* Trim the excess space in the convex hull array */
     hull->p = (point_t*)realloc(hull->p, (hull->n) * sizeof(*(hull->p)));
     assert(hull->p); 
 }
 
-
-int main( void )
-{
+int main( void ) {
     points_t pset, hull;
     double tstart, elapsed;
     
